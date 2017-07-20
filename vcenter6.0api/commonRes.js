d35dbd2b5fa07dfc505e7ad87cc4982d05719ca5
heidsoft <heidsoft@qq.com>
2017-07-20 23:42:06 +0800

    Add vcenter sdk api

---
+++ vcenter6.0api/commonRes.js
@@ -0,0 +1,16 @@
+/* commonRes.js - The variables in this file are used in the file vim/vmodl/doc/right-pane.html */
+/* Each page footer in the API Referemce also uses the ID_Copyright and ID_VersionInformation variables. */
+/* Whenever you change the variable values for a release, change only the date and the version number itself. The variable values are embedded in text. Do not add additional text. */
+/* Version numbers will be of the form "5.5 Beta" or "6.0". /*
+
+/* ID_Copyright--embedded in the footer for each page. */
+var ID_Copyright = "&copy; 2011-2015 VMware, Inc. All rights reserved.";
+
+/* ID_VersionInformation--embedded in the footer for each page; contains the release date and version. */
+var ID_VersionInformation = "Revision 15-Jan-2015 &nbsp;|&nbsp; VMware vSphere API &nbsp; | &nbsp; Version 6.0";
+
+/* ID_VersionRelease--embedded in the "What's New ..." header. */
+var ID_VersionRelease = "6.0";
+
+/* ID_Version--embedded in the explanation of the "Since ..." label. */
+var ID_Version = "6.0";
